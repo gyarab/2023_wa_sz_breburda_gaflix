bfe2932cd867d8032abc99b53bf410f094da18e8
Krystof <krystof.breburda@student.gyarab.cz>
2024-04-26 09:24:13 +0200

    UwU

--- filmy/models.py
+++ filmy/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
 class Movie(models.Model):
-    title = models.CharField(max_length=300)
-    year = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=300)
+    year = models.PositiveSmallIntegerField(blank = True, null = True)
+    footage = models.PositiveSmallIntegerField(blank = True, null = True, help_text = "in minutes")
+
+    def __str__(self):
+        return self.name
+    
+class Director(models.Model):
+    name = models.CharField(max_length=300)
+    birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank = True)
+    main_picture = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=300)
+    
+    def __str__(self) -> str:
+        return self.name
