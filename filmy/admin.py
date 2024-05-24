9b9c5b149fb9b8c7feba9d536e5bd539ca0747b6
Krystof <krystof.breburda@student.gyarab.cz>
2024-05-24 09:27:38 +0200

    UwU

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from filmy.models import Movie, Director, Genre
+from filmy.models import Movie, Director, Genre, Actor
+
+class MovieLoader(admin.ModelAdmin):
+    list_display = ['id', 'name', 'year', 'footage', 'genres_display', 'director']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'director_name']
+    list_filter = ['genres', 'year']
 
 class MovieAdmin(admin.ModelAdmin):
     pass
@@ -10,8 +16,13 @@ class DirectorAdmin(admin.ModelAdmin):
 class GenreAdmin(admin.ModelAdmin):
     pass
 
+class ActorAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Movie, MovieAdmin)
 
 admin.site.register(Director, DirectorAdmin)
 
 admin.site.register(Genre, GenreAdmin)
+
+admin.site.register(Actor, ActorAdmin)
