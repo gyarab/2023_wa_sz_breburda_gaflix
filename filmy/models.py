3c8bc69c946ccfc75fdd9e8698a8c90ba8ddfe3b
Krystof <krystof.breburda@student.gyarab.cz>
2024-05-24 09:14:56 +0200

    fixtures UwU

--- filmy/models.py
+++ filmy/models.py
@@ -4,9 +4,17 @@ class Movie(models.Model):
     name = models.CharField(max_length=300)
     year = models.PositiveSmallIntegerField(blank = True, null = True)
     footage = models.PositiveSmallIntegerField(blank = True, null = True, help_text = "in minutes")
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null=True)
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+    actors = models.ManyToManyField('Actor', blank=True)
+    genres = models.ManyToManyField('Genre', blank=True)
 
     def __str__(self):
         return self.name
+    
+    def gener_display(self):
+        return ", ".join(i.name for i in self.genres.all())
     
 class Director(models.Model):
     name = models.CharField(max_length=300)
@@ -17,6 +25,15 @@ class Director(models.Model):
     def __str__(self):
         return self.name
     
+class Actor(models.Model):
+    name = models.CharField(max_length=300)
+    birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank = True)
+    main_picture = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Genre(models.Model):
     name = models.CharField(max_length=300)
     
